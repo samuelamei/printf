56d06bcdd36093b80a3b4bff4212c9f6f73223a1
sidney theuri <sidneytheuri.st@gmail.com>
2023-03-26 13:40:20 -0700

    compilation commit for a.out

--- main.h
+++ main.h
@@ -7,14 +7,14 @@
 #define UNUSED(x) (void)(x)
 #define BUFF_SIZE 1024
 
-/*************** THIS IS FOR FLAGS ***************/
+/* FLAGS */
 #define F_MINUS 1
 #define F_PLUS 2
 #define F_ZERO 4
 #define F_HASH 8
 #define F_SPACE 16
 
-/*************** THIS IS FOR SIZES ***************/
+/* SIZES */
 #define S_LONG 2
 #define S_SHORT 1
 
@@ -43,7 +43,7 @@ int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
 va_list list, char buffer[], int flags, int width, int precision, int size);
 
-/******************THIS IS FOR FUNCTIONS ******************/
+/****************** FUNCTIONS ******************/
 
 /* Funtions to print chars and strings */
 int print_char(va_list types, char buffer[],
@@ -105,7 +105,7 @@ int write_unsgnd(int is_negative, int ind,
 char buffer[],
 	int flags, int width, int precision, int size);
 
-/****************** THIS IS FOR UTILS ******************/
+/****************** UTILS ******************/
 int is_printable(char);
 int append_hexa_code(char, char[], int);
 int is_digit(char);
